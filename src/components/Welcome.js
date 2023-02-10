6cbb0537c9d961d3f7eb93cef3edf1b0c0dd3192
Kartik Pal <114161683+kartikp962@users.noreply.github.com>
2023-02-10 21:49:19 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+import React from "react";
+import "../styles/App.css";
+import Welcome from "./Welcome";
+
+const App = () => {
+  return <Welcome name={"Suman"}></Welcome>;
+};
+
+export default App;
